8a2f72231d09aec664a517e3602061eb45e4b014
DenizKu <deniz.kuecuekkus@gmail.com>
2018-05-15 18:08:51 +0200

    add project Task2

---
+++ tasks/Task2/Task2/Fussball.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Fussball
+    {
+        private string team1, team2, uhrzeit;
+        private bool findetstatt;
+
+        public Fussball(string kteam1, string kteam2, string kuhrzeit, bool kstaat)
+        {
+            team1 = kteam1;
+            team2 = kteam2;
+            uhrzeit = kuhrzeit;
+            findetstatt = kstaat;
+        }
+
+        public string ChangeUhrzeit
+        {
+            get
+            {
+                return uhrzeit;
+            }
+            set
+            {
+                uhrzeit = value;
+            }
+        }
+
+        public void findetStatt(bool fstaat)
+        {
+            if (fstaat)
+            {
+                Console.WriteLine("Das Spiel findet statt");
+            }
+            else
+            {
+                Console.WriteLine("Das Spiel ist abgesagt");
+            }
+        }
+
+    }
+}
---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fussball fb = new Fussball("Real Madrid", "Barcelona", "20:45", true);
+
+            Console.WriteLine(fb.ChangeUhrzeit);
+            Console.WriteLine(fb.ChangeUhrzeit = "21:45");
+            Console.WriteLine(fb.ChangeUhrzeit);
+
+        }
+    }
+}
