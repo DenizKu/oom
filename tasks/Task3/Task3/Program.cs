de73c2636b4859e64311cc5833115d2ec834bb39
DenizKu <deniz.kuecuekkus@gmail.com>
2018-06-04 23:24:45 +0200

    array hinzugefügt in Task3)

--- tasks/Task3/Task3/Program.cs
+++ tasks/Task3/Task3/Program.cs
@@ -12,9 +12,13 @@ namespace Task3
         {
             IZoohandlungService K1 = new KundeIn(01,"Max","Mustermann","Musteradresse");
             IZoohandlungService M1 = new MitarbeiterIn(05, 'V', 1500.00, "Klaus", "Klaas", "Klausgasse");
+            IZoohandlungService P1 = new KundeIn(02, "Martin", "Martaus", "Martangasse");
+            IZoohandlungService[] allObjectZoo = { K1, M1, P1};
+            foreach (IZoohandlungService I in allObjectZoo)
+            {
+                I.IchBinEin();              
+            }
 
-            K1.IchBinEin();
-            M1.IchBinEin();
         }
     }
 }
---
+++ tasks/task4/task4/Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace task4
+{
+    [TestFixture]
+
+    class Tests
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+
+        [Test]
+        public void SimpleStringTest()
+        {
+            String a = "abced";
+            Assert.AreEqual(a, "abced");
+            
+        }
+
+    }
+}
