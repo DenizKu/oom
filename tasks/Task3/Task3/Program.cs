4a6e415335db0baefb34c1e90739b1170bb229e9
DenizKu <deniz.kuecuekkus@gmail.com>
2018-06-26 18:45:24 +0200

    add project Task7

--- tasks/Task3/Task3/Program.cs
+++ tasks/Task3/Task3/Program.cs
@@ -13,6 +13,7 @@ namespace Task3
             IZoohandlungService K1 = new KundeIn(01,"Max","Mustermann","Musteradresse");
             IZoohandlungService M1 = new MitarbeiterIn(05, 'V', 1500.00, "Klaus", "Klaas", "Klausgasse");
             IZoohandlungService P1 = new KundeIn(02, "Martin", "Martaus", "Martangasse");
+
             IZoohandlungService[] allObjectZoo = { K1, M1, P1};
             foreach (IZoohandlungService I in allObjectZoo)
             {
